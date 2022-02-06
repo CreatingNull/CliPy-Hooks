4039d66e07c3d0ed9ab8c9c9f8271dd51f37841a
nulltek <steve.public@nulltek.xyz>
2022-02-06 18:39:07 +1300

    Simplifying the API.

--- pre_commit_pycli/cli.py
+++ pre_commit_pycli/cli.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 """Command Class functionality for calling a CLI tool."""
-import os
 import re
 import shutil
 import subprocess as sp
@@ -14,19 +13,16 @@ from typing import List
 class Command:
     """Super class that all commands inherit."""
 
-    def __init__(self, command: str, look_behind: str, args: List[str], help_url: str):
+    def __init__(self, command: str, args: List[str], help_url: str = ""):
         """Constructor for the cli command class.
 
         :param command: Name of the command, must be on path or define install location.
-        :param look_behind: Defines the text preceding version output.
         :param args: Additional arguments to provide to the CLI tool.
         :param help_url: URL of project documentation to assist when things go wrong.
         """
         self.args = args
-        self.look_behind = look_behind
         self.command = command
-        # Will be [] if not run using pre-commit or if there are no committed files
-        self.files = self.get_added_files()
+        self.files = []
 
         self.stdout = b""
         self.stderr = b""
@@ -34,7 +30,7 @@ class Command:
 
         self.help_url = help_url
         self.install_path = ""
-        self.parse_args(self.args)
+        self.parse_args()
 
     def check_installed(self):
         """Check if command is installed and fail exit if not."""
@@ -46,97 +42,59 @@ class Command:
                 path = None  # Executable not found
         if path is None:
             check_path = (
-                f" at '{self.install_path}'"
+                f"at '{self.install_path}'"
                 if self.install_path != ""
-                else " and on your PATH"
+                else "and on your PATH"
             )
-            details = (
-                f"Make sure {self.command} is installed {check_path}.\n"
-                f"For more info: {self.help_url}"
+            details = f"Make sure {self.command} is installed {check_path}.\n" + (
+                f"For more info: {self.help_url}" if self.help_url != "" else ""
             )
-            self.raise_error(f"{self.command} not found", details)
+            self.__raise_error(f"{self.command} not found", details)
 
-    def get_added_files(self):
-        """Find added files using git."""
-        added_files = sys.argv[1:]  # 1: don't include the hook file
-        # cfg files are used by uncrustify and won't be source files
-        added_files = [
-            f for f in added_files if os.path.exists(f) and not f.endswith(".cfg")
-        ]
-
-        # Taken from:
-        # https://github.com/pre-commit/pre-commit-hooks/blob/master/pre_commit_hooks/util.py
-        # If no files are provided and if this is used as a command,
-        # Find files the same way pre-commit does.
-        if len(added_files) == 0:
-            cmd = ["git", "diff", "--staged", "--name-only", "--diff-filter=A"]
-            sp_child = sp.run(cmd, stdout=sp.PIPE, stderr=sp.PIPE, check=False)
-            if sp_child.stderr or sp_child.returncode != 0:
-                self.raise_error(
-                    "Problem determining which files are being committed using git.",
-                    sp_child.stderr.decode(),
-                )
-            added_files = sp_child.stdout.decode().splitlines()
-        return added_files
-
-    def parse_args(self, args: List[str]):
+    def parse_args(self):
         """Parse the args into usable variables."""
-        self.args = list(args[1:])  # don't include calling function
+        args = self.args
+        args.reverse()  # Start with positional args
+        positional = True
         for arg in args:
-            if arg in self.files and not arg.startswith("-"):
-                self.args.remove(arg)
-            if arg.startswith("--version"):
-                # If --version is passed in as 2 arguments, where the second is version
-                if arg == "--version" and args.index(arg) != len(args) - 1:
-                    expected_version = args[args.index(arg) + 1]
+            if arg.startswith("-"):
+                positional = False
+            if arg.strip() == "":  # Remove empty args
+                del self.args[args.index(arg)]
+            if positional and Path(arg).exists() and Path(arg).is_file():
+                self.files.append(arg)
+                del self.args[args.index(arg)]
+            if arg.startswith("--version") and args.index(arg) > 0:
                 # Expected split of --version=8.0.0 or --version 8.0.0 with as many spaces as needed
-                else:
-                    expected_version = (
-                        arg.replace(" ", "").replace("=", "").replace("--version", "")
-                    )
-                actual_version = self.get_version_str()
-                self.assert_version(actual_version, expected_version)
-            if arg.startswith("--install-path"):
+                actual_version = self._get_version_str()
+                self._assert_version(actual_version, args[args.index(arg) - 1])
+            if arg.startswith("--install-dir") and args.index(arg) > 0:
                 # Special arg for setting absolute installation path.
-                install_path = args[args.index(arg) + 1]
+                install_path = args[args.index(arg) - 1]
                 if not Path(install_path).exists():
-                    self.raise_error(
+                    self.__raise_error(
                         "Install path argument is invalid.",
                         f"The path '{install_path}' does not exist on the system.",
                     )
                 # Resolve relative paths to absolute.
                 self.install_path = Path(install_path).resolve().__str__()
+                # Remove special arguments from call
+                del self.args[args.index(arg) - 1]
+                del self.args[args.index(arg)]
 
-    def add_if_missing(self, new_args: List[str]):
-        """Add a default if it's missing from the command. This library exists
-        to force checking, so prefer those options. len(new_args) should be 1,
-        or 2 for options like --key=value.
-
-        If first arg is missing, add new_args to command's args
-        Do not change an option - in those cases return.
-        """
-        new_arg_key = new_args[0].split("=")[0]
-        for arg in self.args:
-            existing_arg_key = arg.split("=")[0]
-            if existing_arg_key == new_arg_key:
-                return
-        self.args += new_args
-
-    def assert_version(self, actual_ver: str, expected_ver: str):
+    def _assert_version(self, actual_ver: str, expected_ver: str):
         """--version hook arg enforces specific versions of tools."""
-        expected_len = len(expected_ver)  # allows for fuzzy versions
-        if expected_ver not in actual_ver[:expected_len]:
-            problem = "Version of " + self.command + " is wrong"
-            details = (
-                f"Expected version: {expected_ver} Found version: {actual_ver}"
-                "Edit your pre-commit config or use a different version "
-                f"of {self.command}."
-            )
-            self.raise_error(problem, details)
-        # If the version is correct, exit normally
-        sys.exit(0)
+        if expected_ver in actual_ver:
+            return  # If the version is correct, continue execution
+        problem = "Version of " + self.command + " is wrong."
+        details = (
+            f"Expected version: {expected_ver} Found version: {actual_ver}. "
+            "Edit your pre-commit config or use a different version"
+            f"of {self.command}."
+        )
+        self.__raise_error(problem, details)
 
-    def raise_error(self, problem: str, details: str):
+    def __raise_error(self, problem: str, details: str):
         """Raise a formatted error."""
         format_list = [self.command, problem, details]
         stderr_str = """Problem with {}: {}\n{}\n""".format(*format_list)
@@ -144,38 +102,43 @@ class Command:
         self.stderr = stderr_str.encode()
         self.return_code = 1
         sys.stderr.buffer.write(self.stderr)
-        sys.exit(self.return_code)
+        raise SystemExit(self.return_code)
 
-    def get_version_str(self):
-        """Get the version string like 8.0.0 for a given command."""
-        args = [self.install_path + self.command, "--version"]
-        sp_child = sp.run(args, stdout=sp.PIPE, stderr=sp.PIPE, check=False)
+    def _get_version_str(self):
+        """Get the semantic version string for a given command."""
+        sp_child = self._execute_with_arguments(["--version"])
         version_str = str(sp_child.stdout, encoding="utf-8")
         # After version like `8.0.0` is expected to be '\n' or ' '
-        regex = self.look_behind + r"((?:\d+\.)+[\d+_\+\-a-z]+)"
+        # (\d.){1,2}(\d)
+        regex = r"((?:\d+\.)+[\d+_\+\-a-z]+)"
         search = re.search(regex, version_str)
         if not search:
             details = "The version format for this command has changed."
-            self.raise_error("getting version", details)
-        version = search.group(1)
-        return version
+            self.__raise_error("getting version", details)
+        return search.group(1)
+
+    def _execute_with_arguments(self, args) -> sp.CompletedProcess:
+        args = [Path(self.install_path).joinpath(self.command), *args]
+        if args[0].suffix == ".py":  # Run python script
+            args.insert(0, "python")
+        return sp.run(args, stdout=sp.PIPE, stderr=sp.PIPE, check=False)
 
 
 class StaticAnalyzerCmd(Command):
-    """Commmands that analyze code and are not formatters."""
+    """Commands that analyze code and do not modify it."""
 
-    def run_command(self, args: List[str]):
+    def run_command(self) -> bool:
         """Run the command and check for errors.
 
         Args includes options and filepaths
         """
         self.check_installed()
-        args = [self.install_path + self.command, *args]
-        sp_child = sp.run(args, stdout=sp.PIPE, stderr=sp.PIPE, check=False)
+        sp_child = self._execute_with_arguments([*self.args, *self.files])
         self.stdout += sp_child.stdout
         self.stderr += sp_child.stderr
         self.return_code = sp_child.returncode
         self.exit_on_error()
+        return self.return_code == 0
 
     def exit_on_error(self):
         """On non-zero code writes buffered error message and exits.
@@ -184,4 +147,4 @@ class StaticAnalyzerCmd(Command):
         """
         if self.return_code != 0:
             sys.stderr.buffer.write(self.stdout + self.stderr)
-            sys.exit(self.return_code)
+            raise SystemExit(self.return_code)
